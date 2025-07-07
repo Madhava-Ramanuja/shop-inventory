0fbc2f804e199bf7d5da9d6a7396b74dfe40a7e8
Madhava_Ramanuja <ramanuja2006@gmail.com>
2025-07-08 01:35:33 +0530

    Setup for Render deployment

--- db_config.py
+++ db_config.py
@@ -1,10 +1,11 @@
-import mysql.connector
 import os
+import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME")
+        host=os.environ.get("DB_HOST", "mysql.railway.internal"),
+        user="root",
+        password=os.environ.get("DB_PASSWORD", "wEPOFWSbyQvPUpbtnhfJXGMDfrSffvao"),
+        database=os.environ.get("DB_NAME", "railway"),
+        port=3306
     )
